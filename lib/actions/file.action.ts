e72e733008bf3cec93431952a1552b87d0c3d9a6
harita-gr <haritagr19@gmail.com>
2025-07-14 18:52:05 +0900

    implemented search,sort,filter

--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -1,12 +1,11 @@
 "use server";
 
-import { ID, Query } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { getCurrentUser, handleError } from "./user.action";
 import { InputFile } from "node-appwrite/file";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
-import { error } from "console";
 import { revalidatePath } from "next/cache";
 
 export const uploadFile = async ({
@@ -65,7 +64,13 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: any) => {
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
   const queries = [
     Query.or([
       Query.equal("owner", currentUser.$id),
@@ -73,11 +78,27 @@ const createQueries = (currentUser: any) => {
     ]),
   ];
 
-  //TODO: Search, sort, limit etc.
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+    );
+  }
+
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
   try {
     const currentUser = await getCurrentUser();
@@ -85,7 +106,7 @@ export const getFiles = async () => {
       throw new Error("User not found");
     }
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
